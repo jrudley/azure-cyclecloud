6d5729c97f66732d43a934340cd9ca99da255b4b
James Rudley <jrudley@users.noreply.github.com>
2022-02-18 11:24:09 -0600

    Update runpi.sh

--- runpi.sh
+++ runpi.sh
@@ -2,8 +2,9 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 #
-mkdir -p /shared/scratch/pi
-cp ~/demo/pi.py /shared/scratch/pi
-cp ~/demo/pi.sh /shared/scratch/pi
-cd /shared/scratch/pi
-qsub -J 1-1000 /shared/scratch/pi/pi.sh
+#mkdir -p /shared/scratch/pi
+#cp ~/demo/pi.py /shared/scratch/pi
+#cp ~/demo/pi.sh /shared/scratch/pi
+#cd /shared/scratch/pi
+#cd /shared/home/azure-cyclecloud
+sbatch -N 2 -p hpc /shared/home/azure-cyclecloud/pi.sh
