60678110eacc441deed3c0db63cf9c28407587da
James Rudley <jrudley@users.noreply.github.com>
2022-02-18 13:12:22 -0600

    Update grabfiles.sh

--- grabfiles.sh
+++ grabfiles.sh
@@ -1,3 +1,3 @@
-wget -O /shared/home/jrudley https://raw.githubusercontent.com/jrudley/azure-cyclecloud/main/pi.sh
-wget -O /shared/home/jrudley https://raw.githubusercontent.com/jrudley/azure-cyclecloud/main/pi.py
+wget -P /shared/home/jrudley https://raw.githubusercontent.com/jrudley/azure-cyclecloud/main/pi.sh
+wget -P /shared/home/jrudley https://raw.githubusercontent.com/jrudley/azure-cyclecloud/main/pi.py
 chmod 750 pi.sh pi.py
