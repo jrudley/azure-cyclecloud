858cfc6e261d2188bb89b9409c6181d95e5084cc
James Rudley <jrudley@users.noreply.github.com>
2022-02-18 12:05:30 -0600

    Update runpi.sh

--- runpi.sh
+++ runpi.sh
@@ -4,4 +4,4 @@
 #
 sudo mkdir -p /shared/scratch/pi
 sudo cp -r /tmp/azure-cyclecloud/. /shared/scratch/pi
-sbatch -N 2 -p hpc /shared/scratch/pi/pi.sh
+sudo sbatch -N 2 -p hpc /shared/scratch/pi/pi.sh
